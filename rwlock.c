11eb690a4fc063463123f9a60967e80a0ce68413
Ishfaq-code <ishfaq.mohsin.main@gmail.com>
2026-04-16 11:00:57 -0400

    feat: rw locks

---
+++ rwlock.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+int main(void) {
+    printf("hello\n");
+    return 0;
+}
---
+++ rwlock.h
@@ -0,0 +1,19 @@
+#ifndef RW_LOCK_H
+#define RW_LOCK_H
+
+#include <semaphore.h>
+
+
+typedef struct _rwlock_t {
+    sem_t writelock;
+    sem_t lock;
+    int readers;
+} rwlock_t;
+
+void rwlock_init(rwlock_t *lock);
+void rwlock_acquire_readlock(rwlock_t *lock);
+void rwlock_release_readlock(rwlock_t *lock);
+void rwlock_acquire_writelock(rwlock_t *lock);
+void rwlock_release_writelock(rwlock_t *lock);
+
+#endif
