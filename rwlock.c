37bf7047ca26d869801b90bf59efb4289c87796d
Ishfaq-code <ishfaq.mohsin.main@gmail.com>
2026-04-16 11:03:14 -0400

    fix: rw locks

--- rwlock.c
+++ rwlock.c
@@ -2,8 +2,41 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <semaphore.h>
 
-int main(void) {
-    printf("hello\n");
-    return 0;
+#include "rwlock.h"
+
+
+void rwlock_init(rwlock_t *lock){
+    lock->readers = 0;
+    Sem_init(&lock->lock, 1); 
+    Sem_init(&lock->writelock, 1); 
+}
+
+
+void rwlock_acquire_readlock(rwlock_t *lock){
+    Sem_wait(&lock->lock);
+    lock->readers++;
+    if (lock->readers == 1){
+	    Sem_wait(&lock->writelock);
+    }
+    Sem_post(&lock->lock);
+}
+
+void rwlock_release_readlock(rwlock_t *lock){
+    Sem_wait(&lock->lock);
+    lock->readers--;
+    if (lock->readers == 0){
+        Sem_post(&lock->writelock);
+    }
+    Sem_post(&lock->lock);
+}
+
+void rwlock_acquire_writelock(rwlock_t *lock){
+    Sem_wait(&lock->writelock);
+
+}
+
+void rwlock_release_writelock(rwlock_t *lock){
+    Sem_post(&lock->writelock);
 }
